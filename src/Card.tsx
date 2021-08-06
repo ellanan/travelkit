3b024a582a8b039ccd3cb03a8a5b5274ea1f4ef1
ellanan <38847123+ellanan@users.noreply.github.com>
2021-08-05 22:17:12 -0400

    make card bg transparent and blurred
    Signed-off-by: ellanan <38847123+ellanan@users.noreply.github.com>

--- src/Card.tsx
+++ src/Card.tsx
@@ -5,5 +5,6 @@ export const Card = styled.div`
   margin: 2rem;
   box-shadow: 0 5px 10px 1px rgba(0, 0, 0, 0.2);
   border-radius: 10px;
-  background-color: #d1c7f3;
+  background-color: #d1c7f3f5;
+  backdrop-filter: blur(1px);
 `;
