7131da8b6d271d3055e89b0ee2c4d74232c04c22
ellanan <38847123+ellanan@users.noreply.github.com>
2021-08-09 10:40:14 -0400

    add createNewCategory feature
    Signed-off-by: ellanan <38847123+ellanan@users.noreply.github.com>

--- src/List.tsx
+++ src/List.tsx
@@ -1,4 +1,5 @@
 import produce from 'immer';
+import styled from 'styled-components/macro';
 import {
   DragDropContext,
   Droppable,
@@ -17,6 +18,7 @@ import { useListData } from './useListData';
 const List = ({ id }: { id: string }) => {
   const {
     listData,
+    createNewCategory,
     createNewItemInCategory,
     setItemInCategoryChecked,
     setItemInCategoryName,
@@ -90,90 +92,101 @@ const List = ({ id }: { id: string }) => {
             }}
           >
             {listData?.categories.map((category) => (
-              <Card as='li' key={category.id}>
-                <Input
-                  type='text'
-                  defaultValue={category.name}
-                  variant='unstyled'
-                  onBlur={(e) => {
-                    if (category.name !== e.target.value) {
-                      setCategoryName({
-                        categoryId: category.id,
-                        name: e.target.value,
-                      });
+              <StyledListItem key={category.id}>
+                <Card style={{ width: '100%' }}>
+                  <Input
+                    type='text'
+                    defaultValue={category.name}
+                    variant='unstyled'
+                    onBlur={(e) => {
+                      if (category.name !== e.target.value) {
+                        setCategoryName({
+                          categoryId: category.id,
+                          name: e.target.value,
+                        });
+                      }
+                    }}
+                  />
+
+                  <button
+                    onClick={() =>
+                      createNewItemInCategory({ categoryId: category.id })
                     }
-                  }}
-                ></Input>
-
-                <button
-                  onClick={() =>
-                    createNewItemInCategory({ categoryId: category.id })
-                  }
-                >
-                  +
-                </button>
-                <Droppable droppableId={category.id}>
-                  {(provided, snapshot) => (
-                    <ol
-                      ref={provided.innerRef}
-                      style={{ listStyleType: 'none', padding: 0 }}
-                    >
-                      {category.items.map((item, index) => {
-                        return (
-                          <Draggable
-                            key={item.id}
-                            draggableId={item.id}
-                            index={index}
-                          >
-                            {(provided, snapshot) => (
-                              <HStack
-                                as='li'
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                                // @ts-ignore
-                                style={getItemStyle(
-                                  snapshot.isDragging,
-                                  provided.draggableProps.style
-                                )}
-                              >
-                                <DragHandleIcon />
-                                <Checkbox
-                                  isChecked={item.checked}
-                                  onChange={(e) => {
-                                    setItemInCategoryChecked({
-                                      categoryId: category.id,
-                                      itemId: item.id,
-                                      checked: e.target.checked,
-                                    });
-                                  }}
-                                />
-
-                                <Input
-                                  type='text'
-                                  defaultValue={item.name}
-                                  variant='unstyled'
-                                  onBlur={(e) => {
-                                    if (item.name !== e.target.value) {
-                                      setItemInCategoryName({
+                  >
+                    +
+                  </button>
+                  <Droppable droppableId={category.id}>
+                    {(provided, snapshot) => (
+                      <ol
+                        ref={provided.innerRef}
+                        style={{ listStyleType: 'none', padding: 0 }}
+                      >
+                        {category.items.map((item, index) => {
+                          return (
+                            <Draggable
+                              key={item.id}
+                              draggableId={item.id}
+                              index={index}
+                            >
+                              {(provided, snapshot) => (
+                                <HStack
+                                  as='li'
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  // @ts-ignore
+                                  style={getItemStyle(
+                                    snapshot.isDragging,
+                                    provided.draggableProps.style
+                                  )}
+                                >
+                                  <DragHandleIcon />
+                                  <Checkbox
+                                    isChecked={item.checked}
+                                    onChange={(e) => {
+                                      setItemInCategoryChecked({
                                         categoryId: category.id,
                                         itemId: item.id,
-                                        name: e.target.value,
+                                        checked: e.target.checked,
                                       });
-                                    }
-                                  }}
-                                />
-                              </HStack>
-                            )}
-                          </Draggable>
-                        );
-                      })}
-                      {provided.placeholder}
-                    </ol>
-                  )}
-                </Droppable>
-              </Card>
+                                    }}
+                                  />
+
+                                  <Input
+                                    type='text'
+                                    defaultValue={item.name}
+                                    variant='unstyled'
+                                    onBlur={(e) => {
+                                      if (item.name !== e.target.value) {
+                                        setItemInCategoryName({
+                                          categoryId: category.id,
+                                          itemId: item.id,
+                                          name: e.target.value,
+                                        });
+                                      }
+                                    }}
+                                  />
+                                </HStack>
+                              )}
+                            </Draggable>
+                          );
+                        })}
+                        {provided.placeholder}
+                      </ol>
+                    )}
+                  </Droppable>
+                </Card>
+              </StyledListItem>
             ))}
+            <StyledListItem style={{ width: '25%', padding: '1em' }}>
+              <Card
+                as='button'
+                onClick={createNewCategory}
+                style={{ width: '100%', minHeight: '80px' }}
+              >
+                +
+              </Card>
+            </StyledListItem>
           </ol>
         </DragDropContext>
       </div>
@@ -196,4 +209,11 @@ const getItemStyle = (
   ...draggableStyle,
 });
 
+const StyledListItem = styled.li`
+  width: 100%;
+  padding: 1em;
+  @media (min-width: 600px) {
+    width: 25%;
+  }
+`;
 export default List;
