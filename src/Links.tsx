24e5d543d3417c88cb1b6429a6e96136020247d3
ellanan <38847123+ellanan@users.noreply.github.com>
2021-08-17 17:21:16 -0400

    add home page links
    Signed-off-by: ellanan <38847123+ellanan@users.noreply.github.com>

--- src/Links.tsx
+++ src/Links.tsx
@@ -3,7 +3,7 @@ import { GoMarkGithub } from 'react-icons/go';
 import { FaTwitter } from 'react-icons/fa';
 import { HStack } from '@chakra-ui/react';
 
-const Links = () => {
+export const ListPage = () => {
   return (
     <ul
       style={{
@@ -51,4 +51,50 @@ const Links = () => {
   );
 };
 
-export default Links;
+export const HomePage = () => {
+  return (
+    <ul
+      style={{
+        padding: '1em',
+        listStyleType: 'none',
+        marginTop: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <li
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <HStack gridGap={4}>
+          <a href='https://github.com' target='_blank' rel='noreferrer'>
+            <GoMarkGithub size={18} />
+          </a>
+          <a href='https://twitter.com/hpme' target='_blank' rel='noreferrer'>
+            <FaTwitter size={18} color='#1DA1F2' />
+          </a>
+        </HStack>
+      </li>
+
+      <li>
+        <a
+          href='https://www.freepik.com/vectors/people'
+          style={{
+            color: '#333',
+            fontSize: '10px',
+            textDecoration: 'none',
+            opacity: 0.4,
+            fontFamily: 'sans-serif',
+          }}
+          target='_blank'
+          rel='noreferrer'
+        >
+          People vector created by pch.vector - www.freepik.com
+        </a>
+      </li>
+    </ul>
+  );
+};
