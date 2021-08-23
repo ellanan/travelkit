1b2bc7b376054508928fe0f443781285efe6f600
ellanan <38847123+ellanan@users.noreply.github.com>
2021-08-23 17:19:05 -0400

    update home page to accommodate smaller screen
    Signed-off-by: ellanan <38847123+ellanan@users.noreply.github.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,13 +28,13 @@ const Home = () => {
             font-size: 50px;
             font-weight: bold;
             text-align: left;
-            margin: 20px 50px;
+            margin: 20px 40px;
             padding: 20px;
             @media (max-width: 800px) {
               font-size: 30px;
               font-weight: bold;
               text-align: center;
-              margin: 10px 24px;
+              margin: 10px 20px;
               padding: 10px;
             }
           `}
@@ -70,61 +70,3 @@ const Home = () => {
 };
 
 export default Home;
-
-// *************************************************************************
-// import React, { Fragment } from 'react';
-// import Typewriter from 'typewriter-effect';
-// import MainHeader from '../MainHeader';
-
-// import styled from 'styled-components/macro';
-// import type {} from 'styled-components/cssprop';
-
-// import { HomePageLinks } from '../Links';
-// import { useListData } from '../useListData';
-// import List from '../List';
-
-// const Home = () => {
-//   const { listData, dispatchListAction } = useListData();
-
-//   return (
-//     <Fragment>
-//       <MainHeader />
-//       <div
-//         style={{ display: 'flex', flexDirection: 'row' }}
-//         css={`
-//           display: flex;
-//           flex-direction: row;
-//           background-color: red;
-//         `}
-//       >
-//         <span
-//           style={{
-//             fontSize: '60px',
-//             fontWeight: 'bold',
-//             textAlign: 'left',
-//             margin: '20px 50px',
-//             padding: '20px',
-//           }}
-//         >
-//           Have you packed the
-//           <Typewriter
-//             options={{
-//               strings: ['passport?', 'hand sanitizer?', 'phone charger?'],
-//               autoStart: true,
-//               loop: true,
-//             }}
-//           />
-//         </span>
-//         <img
-//           src={require('../images/travel-checklist.jpg').default}
-//           alt='travel checklist'
-//           style={{ width: '500px', height: '400px' }}
-//         />
-//       </div>
-//       <List listData={listData} dispatchListAction={dispatchListAction} />
-//       <HomePageLinks />
-//     </Fragment>
-//   );
-// };
-
-// export default Home;
