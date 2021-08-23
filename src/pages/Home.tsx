7bf7a71a8f39adfa1de30cf24b565f898fd8316c
ellanan <38847123+ellanan@users.noreply.github.com>
2021-08-23 17:18:48 -0400

    update home page to accommodate smaller screen
    Signed-off-by: ellanan <38847123+ellanan@users.noreply.github.com>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,10 @@
 import React, { Fragment } from 'react';
 import Typewriter from 'typewriter-effect';
 import MainHeader from '../MainHeader';
+
+import styled from 'styled-components/macro';
+import type {} from 'styled-components/cssprop';
+
 import { HomePageLinks } from '../Links';
 import { useListData } from '../useListData';
 import List from '../List';
@@ -11,15 +15,29 @@ const Home = () => {
   return (
     <Fragment>
       <MainHeader />
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div
+        css={`
+          display: flex;
+          @media (max-width: 800px) {
+            flex-direction: column;
+          }
+        `}
+      >
         <span
-          style={{
-            fontSize: '60px',
-            fontWeight: 'bold',
-            textAlign: 'left',
-            margin: '20px 50px',
-            padding: '20px',
-          }}
+          css={`
+            font-size: 50px;
+            font-weight: bold;
+            text-align: left;
+            margin: 20px 50px;
+            padding: 20px;
+            @media (max-width: 800px) {
+              font-size: 30px;
+              font-weight: bold;
+              text-align: center;
+              margin: 10px 24px;
+              padding: 10px;
+            }
+          `}
         >
           Have you packed the
           <Typewriter
@@ -33,7 +51,16 @@ const Home = () => {
         <img
           src={require('../images/travel-checklist.jpg').default}
           alt='travel checklist'
-          style={{ width: '500px', height: '400px' }}
+          css={`
+            width: 500px;
+            height: 400px;
+            @media (max-width: 800px) {
+              width: 350px;
+              height: 300px;
+              margin-left: auto;
+              margin-right: auto;
+            } ;
+          `}
         />
       </div>
       <List listData={listData} dispatchListAction={dispatchListAction} />
@@ -43,3 +70,61 @@ const Home = () => {
 };
 
 export default Home;
+
+// *************************************************************************
+// import React, { Fragment } from 'react';
+// import Typewriter from 'typewriter-effect';
+// import MainHeader from '../MainHeader';
+
+// import styled from 'styled-components/macro';
+// import type {} from 'styled-components/cssprop';
+
+// import { HomePageLinks } from '../Links';
+// import { useListData } from '../useListData';
+// import List from '../List';
+
+// const Home = () => {
+//   const { listData, dispatchListAction } = useListData();
+
+//   return (
+//     <Fragment>
+//       <MainHeader />
+//       <div
+//         style={{ display: 'flex', flexDirection: 'row' }}
+//         css={`
+//           display: flex;
+//           flex-direction: row;
+//           background-color: red;
+//         `}
+//       >
+//         <span
+//           style={{
+//             fontSize: '60px',
+//             fontWeight: 'bold',
+//             textAlign: 'left',
+//             margin: '20px 50px',
+//             padding: '20px',
+//           }}
+//         >
+//           Have you packed the
+//           <Typewriter
+//             options={{
+//               strings: ['passport?', 'hand sanitizer?', 'phone charger?'],
+//               autoStart: true,
+//               loop: true,
+//             }}
+//           />
+//         </span>
+//         <img
+//           src={require('../images/travel-checklist.jpg').default}
+//           alt='travel checklist'
+//           style={{ width: '500px', height: '400px' }}
+//         />
+//       </div>
+//       <List listData={listData} dispatchListAction={dispatchListAction} />
+//       <HomePageLinks />
+//     </Fragment>
+//   );
+// };
+
+// export default Home;
