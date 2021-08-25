a0c5e2cc7a21c2767bba1d0a9aa12764a966e594
ellanan <38847123+ellanan@users.noreply.github.com>
2021-08-25 14:33:05 -0400

    update demo list data
    Signed-off-by: ellanan <38847123+ellanan@users.noreply.github.com>

--- src/pages/demoListData.ts
+++ src/pages/demoListData.ts
@@ -5,41 +5,267 @@ export const demoListData: ListData = {
   categories: [
     {
       id: 'catagory-a',
-      name: 'thing',
+      name: 'travel documents',
       items: [
         {
-          id: 'item-1',
+          id: 'item-a01',
           checked: false,
-          name: 'thingathing',
+          name: 'passport',
+        },
+        {
+          id: 'item-a02',
+          checked: false,
+          name: 'copies of passport',
+        },
+        {
+          id: 'item-a03',
+          checked: false,
+          name: 'drivers license',
+        },
+        {
+          id: 'item-a04',
+          checked: false,
+          name: 'travel insurance',
+        },
+        {
+          id: 'item-a05',
+          checked: false,
+          name: 'vaccination card',
+        },
+        {
+          id: 'item-a06',
+          checked: false,
+          name: 'cash and credit cards',
         },
       ],
-      color: '#FFEEAD',
+      color: '#53DAC2',
     },
     {
       id: 'catagory-b',
-      name: 'ahhhhhhh',
+      name: 'covid must-haves',
       items: [
         {
-          id: 'item-2',
+          id: 'item-b01',
           checked: false,
-          name: 'bazinga',
+          name: 'face mask',
+        },
+        {
+          id: 'item-b02',
+          checked: false,
+          name: 'hand sanitizer',
+        },
+        {
+          id: 'item-b03',
+          checked: false,
+          name: 'antibacterial wipes',
+        },
+        {
+          id: 'item-b04',
+          checked: false,
+          name: 'latex gloves',
         },
       ],
       color: '#96DAC8',
     },
     {
       id: 'catagory-c',
-      name: 'thing',
+      name: 'electronics',
       items: [
         {
-          id: 'item-3',
+          id: 'item-c01',
           checked: false,
-          name: 'thingathing',
+          name: 'laptop & charger',
         },
         {
-          id: 'item-4',
+          id: 'item-c02',
           checked: false,
-          name: 'thingathing',
+          name: 'phone & charger',
+        },
+        {
+          id: 'item-c03',
+          checked: false,
+          name: 'camera & charger',
+        },
+        {
+          id: 'item-c04',
+          checked: false,
+          name: 'e-reader',
+        },
+        {
+          id: 'item-c05',
+          checked: false,
+          name: 'headphones',
+        },
+        {
+          id: 'item-c06',
+          checked: false,
+          name: 'travel adaptor',
+        },
+        {
+          id: 'item-c07',
+          checked: false,
+          name: 'portable power bank',
+        },
+      ],
+      color: '#FFEEAD',
+    },
+    {
+      id: 'catagory-d',
+      name: 'medication',
+      items: [
+        {
+          id: 'item-d01',
+          checked: false,
+          name: 'prescriptions',
+        },
+        {
+          id: 'item-d02',
+          checked: false,
+          name: 'pain relief',
+        },
+        {
+          id: 'item-d03',
+          checked: false,
+          name: 'allergy pills',
+        },
+        {
+          id: 'item-d04',
+          checked: false,
+          name: 'bandaid',
+        },
+        {
+          id: 'item-d05',
+          checked: false,
+          name: 'eyedrop',
+        },
+      ],
+      color: '#D6EFF7',
+    },
+    {
+      id: 'catagory-e',
+      name: 'clothes',
+      items: [
+        {
+          id: 'item-e01',
+          checked: false,
+          name: 't-shirts',
+        },
+        {
+          id: 'item-e02',
+          checked: false,
+          name: 'jeans',
+        },
+        {
+          id: 'item-e03',
+          checked: false,
+          name: 'shorts',
+        },
+        {
+          id: 'item-e04',
+          checked: false,
+          name: 'pajamas',
+        },
+        {
+          id: 'item-e05',
+          checked: false,
+          name: 'swimsuits',
+        },
+        {
+          id: 'item-e06',
+          checked: false,
+          name: 'underwear',
+        },
+        {
+          id: 'item-e07',
+          checked: false,
+          name: 'socks',
+        },
+        {
+          id: 'item-e08',
+          checked: false,
+          name: 'flip flops',
+        },
+        {
+          id: 'item-e09',
+          checked: false,
+          name: 'sandals',
+        },
+        {
+          id: 'item-e10',
+          checked: false,
+          name: 'hiking boots',
+        },
+      ],
+      color: '#D6EFF7',
+    },
+    {
+      id: 'catagory-f',
+      name: 'toiletries',
+      items: [
+        {
+          id: 'item-f01',
+          checked: false,
+          name: 'shampoo',
+        },
+        {
+          id: 'item-f02',
+          checked: false,
+          name: 'conditioner',
+        },
+        {
+          id: 'item-f03',
+          checked: false,
+          name: 'body wash',
+        },
+        {
+          id: 'item-f04',
+          checked: false,
+          name: 'face wash',
+        },
+        {
+          id: 'item-f05',
+          checked: false,
+          name: 'face lotion',
+        },
+        {
+          id: 'item-f06',
+          checked: false,
+          name: 'toothbrush',
+        },
+        {
+          id: 'item-f07',
+          checked: false,
+          name: 'toothpaste',
+        },
+        {
+          id: 'item-f08',
+          checked: false,
+          name: 'floss',
+        },
+        {
+          id: 'item-f09',
+          checked: false,
+          name: 'retainer',
+        },
+        {
+          id: 'item-f10',
+          checked: false,
+          name: 'hair dryer',
+        },
+        {
+          id: 'item-f11',
+          checked: false,
+          name: 'sunscreen',
+        },
+        {
+          id: 'item-f12',
+          checked: false,
+          name: 'contacts',
+        },
+        {
+          id: 'item-f13',
+          checked: false,
+          name: 'glasses',
         },
       ],
       color: '#D6EFF7',
